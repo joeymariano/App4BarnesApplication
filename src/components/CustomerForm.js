6742e2b6ba98ccdecad79f8bd1f1080eddc63f56
joeymariano <joey.michalina.mariano@gmail.com>
2019-11-26 15:04:16 -0500

    added validation function

--- src/components/CustomerForm.js
+++ src/components/CustomerForm.js
@@ -1,7 +1,5 @@
-// customer form
-
-// Name (cannot be empty)
-// Phone Number (at least 7 digits, only numbers)
+// Name (cannot be empty) X
+// Phone Number (at least 7 digits, only numbers) 
 // Email Address (cannot be empty and must be in format abc@xyz.someTld)
 // Zip Code (at least 5 digits, only numbers)
 
@@ -24,6 +22,57 @@ class CustomerForm extends Component {
       [event.target.name]: event.target.value
     })
   }
+
+  
+	handleFormValidation() {    
+	    const { firstName, lastName, phone, email, zipcode } = this.state;    
+	    let formErrors = {};    
+	    let formIsValid = true;    
+
+	    //frist name     
+	    if (!firstName) {    
+	        formIsValid = false;    
+	        formErrors["firstNameErr"] = "First Name is required.";    
+	    }    
+
+	   	//last name     
+	    if (!lastName) {    
+	        formIsValid = false;    
+	        formErrors["lastNameErr"] = "Last Name is required.";    
+	    }    
+
+	    //Email    
+	    if (!email) {    
+	        formIsValid = false;    
+	        formErrors["emailErr"] = "Email is required.";    
+	    }    
+	    else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {    
+	        formIsValid = false;    
+	        formErrors["emailErr"] = "Invalid email.";    
+	    }    
+
+	    //Phone number    
+	    if (!phone) {    
+	        formIsValid = false;    
+	        formErrors["phoneErr"] = "Phone number is required.";    
+	    }    
+	    else {    
+	        var phoneRegex = /^(?:(?:\\+|0{0,2})91(\s*[\\-]\s*)?|[0]?)?[789]\d{9}$/;    
+	        if (!phoneRegex.test(phone)) {    
+	            formIsValid = false;    
+	            formErrors["phoneNumberErr"] = "Invalid phone number.";    
+	        }    
+	    }    
+
+	    //City    
+	    // if (city === '' || city === "select") {    
+	    //     formIsValid = false;    
+	    //     formErrors["cityErr"] = "Select city.";    
+	    // }    
+
+	    this.setState({ formErrors: formErrors });    
+	    return formIsValid;    
+	}      
 
   render() {
     return (
