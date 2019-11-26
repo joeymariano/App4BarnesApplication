a03609b96cf1cea682ce347555d70332763b44fb
joeymariano <joey.michalina.mariano@gmail.com>
2019-11-26 13:35:43 -0500

    form inputs with labels

--- src/components/CustomerForm.js
+++ src/components/CustomerForm.js
@@ -10,18 +10,35 @@ import React, { Component } from 'react';
 class CustomerForm extends Component {
   constructor(props) {
     super(props);
-    this.myPassword = React.createRef()
     this.state = {
-      name: '',
+      firstName: '',
+      lastName: '',
       phone: '',
       email: '',
       zipCode: ''
-    }
+    };
+  }
+
+  updateState = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
   render() {
     return (
-    	<div></div>
+      <form>
+        <label>First Name</label>
+        <input id="firstName" name="firstName" type="text" value={ this.state.firstName } onChange={ this.updateState }/>
+        <label>Last Name</label>
+        <input id="lastName" name="lastName" type="text" value={ this.state.lastName } onChange={ this.updateState }/>
+        <label>Phone</label>
+        <input id="phone" name="phone" type="text" value={ this.state.phone } onChange={ this.updateState }/>
+		<label>Email</label>
+        <input id="email" name="email" type="text" value={ this.state.email } onChange={ this.updateState }/>
+        <label>ZipCode</label>
+        <input id="zipCode" name="zipCode" type="text" value={ this.state.zipCode } onChange={ this.updateState }/>
+      </form>
     )
   }
 }
