d57e4009fa4bae9e505c984d872f75036ccbb18d
joeymariano <joey.michalina.mariano@gmail.com>
2019-11-30 20:05:49 -0500

    hero info now displays proper

---
+++ src/components/HeroCard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Card, CardBody } from 'reactstrap';
+
+class HeroCard extends Component {
+	render(){
+		return(
+			<li>
+				<Card>
+					<CardBody>
+					</CardBody>
+				</Card>
+			</li>
+		)
+	}
+}
+
+export default HeroCard
--- src/components/HeroService.js
+++ src/components/HeroService.js
@@ -8,23 +8,25 @@ import { Button } from 'reactstrap';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as actions from '../actions/act'
+import HeroCard from './HeroCard'
+import TeamInfoCard from './TeamInfoCard'
 
 class HeroService extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-		  visible: true
-	 	}
 
-	 	this.initialState = this.state;    
+	 	}  
 	}
 
-	updateState = (event) => {
-	 this.setState({
-	   [event.target.name]: event.target.value
-	 })
-	}
-
+// [
+// 	{"name":"Molecule Man","age":29,"secretIdentity":"Dan Jukes",
+// 		"powers":["Radiation resistance","Turning tiny","Radiation blast"]},
+// 	{"name":"Madame Uppercut","age":39,"secretIdentity":"Jane Wilson",
+// 		"powers":["Million tonne punch","Damage resistance","Superhuman reflexes"]},
+// 	{"name":"Eternal Flame","age":1000000,"secretIdentity":"Unknown",
+// 		"powers":["Immortality","Heat Immunity","Inferno","Teleportation","Interdimensional travel"]}
+// ]
 	handleSubmit = (event) => {
 		event.preventDefault()
 	}
@@ -37,17 +39,48 @@ class HeroService extends Component {
 		}
 	}
 
-	thisPropsHeros = () => {
-		debugger
-		return this.props.heros
+	renderTeamInfoCards = () => {
+		let result = this.props.info.map((obj, idx) => {
+			let keyValue = Object.keys(obj).toString()
+
+			if (keyValue === 'squadName'){
+				return(
+					<h1 class='heroGroup'>Squad Name: { obj[keyValue] }</h1>
+				)
+			}
+			if (keyValue === 'homeTown'){
+				return(
+					<h2>Hometown: { obj[keyValue] }</h2>
+				)
+			}
+			if (keyValue === 'formed'){
+				return(
+					<p>Formed: { obj[keyValue] } </p>
+				)
+			}
+			if (keyValue === 'secretBase'){
+				return(
+					<p>Secrect Base: { obj[keyValue] }</p>
+				)
+			}
+			if (keyValue === 'active'){
+				return(
+					<p>Active?: { obj[keyValue].toString() }</p>
+				)
+			}
+		})
+		return result
 	}
 
 	render() {
 		return (
 			<div>
-				<Button onClick={ () => { this.props.actions.getHeroService() }}>Get Heros</Button>
-				<p>{ JSON.stringify(this.props.heros) }</p>
-				<p>{ JSON.stringify(this.props.info) }</p>
+				<Button onClick={ () => { this.props.actions.getHeroService() }}>Get More Heros and Team Information.</Button>
+				<br></br>
+				{ this.renderTeamInfoCards() }
+				{ JSON.stringify(this.props.heros) }	
+				{/* need to add my own 4 heros - hard coded */}
+
 			</div>
 		)
 	}
